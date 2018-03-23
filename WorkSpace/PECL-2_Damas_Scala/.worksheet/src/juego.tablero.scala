d3faa2cc7c409679deff1015cc483d26e58419c5
Daras Monkey <davidariza1994@gmail.com>
2018-03-23 20:40:16 +0100

    Cambios de estructura.
    Change-Id: Iafa6809fad3295263437e7b01915e8a69626b2b0
    Signed-off-by: Daras Monkey <davidariza1994@gmail.com>

---
+++ WorkSpace/PECL-2_Damas_Scala/.worksheet/src/juego.tablero.scala
@@ -0,0 +1,5 @@
+package juego
+
+object tablero {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(75); 
+  println("Welcome to the Scala worksheet")}
+}
---
+++ WorkSpace/PECL-2_Damas_Scala/src/juego/tablero.sc
@@ -0,0 +1,5 @@
+package juego
+
+object tablero {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+}
