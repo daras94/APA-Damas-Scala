efab5a24365d17659096a279b5f2699397bb75ac
Daras Monkey <davidariza1994@gmail.com>
2018-03-26 20:55:58 +0200

    Add funcion generrar Tablero. Change-Id: I5608f9ed8cd90a9bc2346c1c2a7fd55fe174320d Signed-off-by: Daras Monkey <davidariza1994@gmail.com>

--- WorkSpace/PECL-2_Damas_Scala/.worksheet/src/juego.tablero.scala
+++
@@ -1,5 +0,0 @@
-package juego
-
-object tablero {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(76); 
-  println("Welcome to the Scala worksheet");}
-}
--- WorkSpace/PECL-2_Damas_Scala/src/juego/tablero.sc
+++
@@ -1,5 +0,0 @@
-package juego
-
-object tablero {
-  println("Welcome to the Scala worksheet");      //> Welcome to the Scala worksheet
-}
