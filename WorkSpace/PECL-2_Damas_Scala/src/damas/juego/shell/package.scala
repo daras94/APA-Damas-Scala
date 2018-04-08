a50e6f40d21691ea54cc7da4821bc91bbd88528b
danieldmp96 <daniel.diazmpl96@gmail.com>
2018-04-08 11:58:14 +0200

    Fix Conflit Merger
    Merge branch 'develop-2' of https://github.com/daras94/APA-Damas-Scala
    into develop-2
    
    Conflicts:
    	WorkSpace/.metadata/.plugins/org.eclipse.e4.workbench/workbench.xmi
    	WorkSpace/.metadata/version.ini
    	WorkSpace/PECL-2_Damas_Scala/.worksheet/src/damas.juego.TestTablero.scala
    	WorkSpace/PECL-2_Damas_Scala/src/damas/juego/Tablero.scala
    	WorkSpace/PECL-2_Damas_Scala/src/damas/juego/TestTablero.sc
    	WorkSpace/PECL-2_Damas_Scala/src/damas/juego/shell/ShellDamas.scala
    	WorkSpace/PECL-2_Damas_Scala/src/damas/juego/shell/ShellSetting.scala
    	WorkSpace/PECL-2_Damas_Scala/src/damas/juego/shell/package.scala
    
    Change-Id: I2d2aba6223d0f18c842d709a81681fc210ef5d0c
    Signed-off-by: Daras Monkey

--- WorkSpace/PECL-2_Damas_Scala/src/damas/juego/shell/package.scala
+++ WorkSpace/PECL-2_Damas_Scala/src/damas/juego/shell/package.scala
@@ -6,14 +6,7 @@ import damas.juego._;
 
   */
 package object shell {
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-=======
-     
->>>>>>> d069b6ad5c46db362f7e9d89b3e725710e232e18
->>>>>>> cc3f73c... Merge branch 'develop-2' of https://github.com/daras94/APA-Damas-Scala into develop-2
      val Tab = Tablero;               // Inicializacion del Tablero.
      val Cfg = ShellSetting;          // Inicializacion de las  configuraciones del juego.
 }
